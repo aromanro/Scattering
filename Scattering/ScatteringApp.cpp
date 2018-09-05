b30c0314ae91c00953de36ef2c9f8f450e23a524
Adrian Roman <aroman_ro@yahoo.com>
2018-09-05 20:16:09 +0300

    Call OnInit from application base class

--- Scattering/ScatteringApp.cpp
+++ Scattering/ScatteringApp.cpp
@@ -7,6 +7,9 @@ wxIMPLEMENT_APP(ScatteringApp);
 
 bool ScatteringApp::OnInit()
 {
+	if (!wxApp::OnInit())
+		return false;
+
 	frame = new ScatteringFrame("Scattering", wxPoint(50, 50), wxSize(1024, 800));
 	frame->Show(true);
 
